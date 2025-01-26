ed0dba4ae2e44e31a0945385601dea203c3884c8
Robert Nicjoo <robertnicjoo@outlook.com>
2025-01-26 10:30:39 +0700

    added new google tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,8 +19,22 @@
         <link rel="stylesheet" href="<?=url('/');?>/assets/css/connectyed.css">        
         @vite(['resources/js/app.js'])
         <!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16739328770"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-16739328770'); </script>
+
+
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WQLSC33H');</script>
+        <!-- End Google Tag Manager -->
     </head>
     <body class="bg-connectyed-body-light">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQLSC33H"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <!-- preloader start here -->
         <div class="preloader">
             <div class="preloader-inner">
